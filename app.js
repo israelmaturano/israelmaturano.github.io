6964d536a6d75cb54f4bf4ed9fa8b3f43176b05c
israelmaturano <55666620+israelmaturano@users.noreply.github.com>
2020-06-16 23:58:20 -0400

    Creating functioning burger

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav_links');
-
-  burger.addEventListener('click',()=>){
+  const navLinks = document.querySelectorAll('.nav_links li');
+  //Toggle Nav
+  burger.addEventListener('click', ()=> {
     nav.classList.toggle('nav_active');
+    //Animate navLinks
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle('toggle');
   });
 }
+
+navSlide();
